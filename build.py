e2611d81e31bd5df0e74cd9dd4ae99a906a85386
neverevis <monfusenpai@gmail.com>
2026-02-03 02:13:33 -0300

    build: add initial python build script

---
+++ build.py
@@ -0,0 +1,12 @@
+import subprocess
+import glob
+
+SOURCE_FOLDER   =   "Lux"
+EXECUTABLE_NAME =   "sandbox.exe"
+COMPILER        =   "clang++"
+CPP_VERSION     =   "-std=c++23"
+
+
+SOURCES = " ".join(glob.glob(f"{SOURCE_FOLDER}/**/*.cpp",recursive=True))
+
+subprocess.run(f"{COMPILER} {SOURCES} -o {EXECUTABLE_NAME} {CPP_VERSION}", shell=True)
