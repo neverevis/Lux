c451a2538d1885db36ec38f50f407f2aa00fe81b
neverevis <monfusenpai@gmail.com>
2026-02-04 23:21:42 -0300

    build: add khronos opengl headers

--- build.py
+++ build.py
@@ -5,8 +5,9 @@ SOURCE_FOLDER   =   "Lux"
 EXECUTABLE_NAME =   "sandbox.exe"
 COMPILER        =   "clang++"
 CPP_VERSION     =   "-std=c++23"
+INCLUDES        =   "Lux/external"
 
 
 SOURCES = " ".join(glob.glob(f"{SOURCE_FOLDER}/**/*.cpp",recursive=True))
 
-subprocess.run(f"{COMPILER} {SOURCES} -o {EXECUTABLE_NAME} {CPP_VERSION} -I{SOURCE_FOLDER} -luser32 -lgdi32 -lopengl32", shell=True)
+subprocess.run(f"{COMPILER} {SOURCES} -o {EXECUTABLE_NAME} {CPP_VERSION} -I{SOURCE_FOLDER} -I{INCLUDES} -luser32 -lgdi32 -lopengl32", shell=True)
